a18ed5e16e2de48aecf5f802670dcf035e5008ef
SonamT <sonam.tamang.mr@gmail.com>
2020-09-25 13:12:30 +0600

    new updates

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-
-var submit = document.getElementById('payroll--submit');
-
-submit.addEventListener('click', function() {
-    var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://raw.githubusercontent.com/SonamTamang90/JSON-data/master/employee-details.json');
-
-    ourRequest.onload = function() {
-        var ourData = JSON.parse(ourRequest.responseText);
-        console.log(ourData);
-    };
-    ourRequest.send();
-});   
-
-
+var DOMstrings = {
+    inputEmployeeName: '.payroll__form-employeename',
+    inputEmployee: '.'
+}
